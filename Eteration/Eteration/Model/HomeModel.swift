8f219cbe06e93edbf5809a1b72d8ec82eb951200
Onur <>
2024-02-02 10:50:27 +0300

    Project folder design completed and project created.

---
+++ Eteration/Eteration/Model/HomeModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeModel.swift
+//  Eteration
+//
+//  Created by Onur on 2.02.2024.
+//
+
+import Foundation
---
+++ Eteration/Eteration/View/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Eteration
+//
+//  Created by Onur on 2.02.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+    }
+}
